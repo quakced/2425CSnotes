cbf5995b348f85397498b5712c5b320ab66e058d
quakced <dabrandle16@gmail.com>
2025-03-18 09:54:42 -0500

    data notes

---
+++ DATA/Notes/Emoji-Matching-Starter/Emoji-Matching-Starter/script.js
@@ -0,0 +1,26 @@
+// create Phaser.Game object assigned to global variable named game
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'my-game', { preload: preload, create: create, update: update });
+
+// declare other global variables (for sprites, etc.)
+
+
+// preload game assets - runs one time when webpage first loads
+function preload() {
+   
+}
+
+// create game world - runs one time after preload finishes
+function create() {
+   
+
+}
+
+// update game - runs repeatedly in loop after create finishes
+function update() {
+    
+}
+
+// add custom functions (for collisions, etc.) - only run when called
+function checkForMatch() {
+    
+}
